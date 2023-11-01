a9fc10bd705930eaf37bfaee561d543f2b2b35c4
Alex <http160790@mail.ru>
2023-11-01 10:11:01 +0700

    andr 96

--- androidApp/build.gradle.kts
+++ androidApp/build.gradle.kts
@@ -15,8 +15,8 @@ android {
         applicationId = "com.medhelp.callmed2"
         minSdk = 24
         targetSdk = 34
-        versionCode = 95
-        versionName = "95"
+        versionCode = 96
+        versionName = "96"
     }
 
     packaging {
